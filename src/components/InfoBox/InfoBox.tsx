295837b4fce6502414bd1e1eb4880b144768db66
Ryssa Sobreira <ryssateles@gmail.com>
2024-05-16 02:35:19 -0300

    Fix: Estrutura do infoBox

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -1,20 +1,14 @@
-// InfoBox.tsx
 import React from 'react';
-import Style from './InfoBox.module.css'
+import Style from './InfoBox.module.css';
 
 const InfoBox = ({ formData, onClose }: { formData: any; onClose: () => void }) => {
     return (
         <div className={Style.infoboxcontainer}>
             <div className={Style.infobox}>
-                <h2>Informações do Formulário</h2>
-                <p><strong>CEP:</strong> {formData.cep}</p>
-                <p><strong>Rua/Logradouro:</strong> {formData.rua}</p>
-                <p><strong>Número:</strong> {formData.numero}</p>
-                <p><strong>Complemento:</strong> {formData.complemento || 'Sem complemento'}</p>
-                <p><strong>Bairro:</strong> {formData.bairro}</p>
-                <p><strong>Cidade:</strong> {formData.cidade}</p>
-                <p><strong>Estado:</strong> {formData.estado}</p>
-                <p><strong>País:</strong> {formData.pais}</p>
+                <h2>Informações do Endereço</h2>
+                <p>{formData.rua}, {formData.numero} - {formData.complemento || 'Sem complemento'}</p>
+                <p>{formData.bairro} - {formData.cidade}, {formData.estado}</p>
+                <p>{formData.cep}, {formData.pais}</p>
                 <button onClick={onClose}>Fechar</button>
             </div>
         </div>
